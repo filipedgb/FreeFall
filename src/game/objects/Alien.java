5701bbcb5bff03684aaee7062749e5c10e54cf0c
ei12068 <ei12068@fe.up.pt>
2014-05-04 02:55:34 +0000

    Logica updated: vida , turbo

--- src/game/objects/Alien.java
+++ src/game/objects/Alien.java
@@ -3,18 +3,39 @@ package game.objects;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.Log;
 import android.view.MotionEvent;
 
 public class Alien extends GameObject {
 
 	private int lifepoints;
-	
-	
+	private double turbopoints;
+	boolean turbo_enabled;
+
+
 	public Alien(int x, int y) {
 		super(x, y, 15, 15);
-		lifepoints = 100;
+		lifepoints = 1000;
+		turbopoints = 100;
+		turbo_enabled = false;
 	}
 	
+	public boolean isTurbo() {
+		return turbo_enabled;
+	}
+	
+	public void setTurbo(boolean a) { 
+		turbo_enabled = a; 
+	}
+	
+	public double getTurbopoints() {
+		return turbopoints;
+	}
+
+	public void setTurbopoints(double d) {
+		this.turbopoints = d;
+	}
+
 	public void moveLeft() {
 		x -= 15;
 	}
@@ -31,8 +52,23 @@ public class Alien extends GameObject {
 		y -= 5;
 	}
 
+	public void getsHit() {
+		lifepoints -= 25;
+		Log.e("vida", "-25 vida");
+		Log.e("vida", "" + lifepoints);
+
+	}
+	
+	public int getLifepoints() {
+		return lifepoints;
+	}
+
+	void setLifepoints(int lifepoints) {
+		this.lifepoints = lifepoints;
+	}
+
 	public void draw(Canvas canvas, Paint paint) {
-		paint.setColor(Color.GREEN);
+		paint.setColor(Color.RED);
 		canvas.drawRect(getX(),getY(),getX()+getWidth(), getY()+getHeight(),paint);
 		// canvas.drawBitmap(bmp, getX(), getY(), paint);
 	}
