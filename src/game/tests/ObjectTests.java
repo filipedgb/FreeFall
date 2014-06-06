607226b6aaa134b9e1faeb4d715fdbad57c36d8c
ei12058 <ei12058@91d1a7ea-e80d-4ec0-ac2e-6a2fed943390>
2014-06-06 01:01:16 +0000

    mais testes

--- src/game/tests/ObjectTests.java
+++ src/game/tests/ObjectTests.java
@@ -1,21 +1,70 @@
 package game.tests;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import game.entities.*;
+
 public class ObjectTests {
-	
+
+	@Test
 	public void HealthItem() {
 		// verificar se o item fica inactivo depois de apanhado (durante x tempo)
-	}
-	
-	public void FuelItem() {
-		// verificar se o item fica inactivo depois de apanhado (durante x tempo)
-	}
-	
-	public void slowDownItem() {
-		// verificar se o item fica inactivo depois de apanhado (durante x tempo)
-	}
-	
-	public void mineItem() {
-		// verificar se o item fica inactivo depois de apanhado (durante x tempo)
+		Player p = new Player(true);
+		Health h = new Health(p.getX(),p.getY(), true);
+
+		assertTrue(h.isActive());
+		h.caught(p, true);
+
+		assertFalse(h.isActive());
 	}
 
+	@Test
+	public void FuelItem() {
+		// verificar se o item fica inactivo depois de apanhado (durante x tempo)
+		Player p = new Player(true);
+		Fuel f = new Fuel(p.getX(),p.getY(), true);
+
+		assertTrue(f.isActive());
+		f.caught(p, true);
+
+		assertFalse(f.isActive());
+	}
+
+	@Test
+	public void slowDownItem() {
+		// verificar se o item fica inactivo depois de apanhado (durante x tempo)
+		Player p = new Player(true);
+		SlowDown sm = new SlowDown(p.getX(),p.getY(), true);
+
+		assertTrue(sm.isActive());
+		sm.caught(p, true);
+
+		assertFalse(sm.isActive());
+	}
+
+	@Test
+	public void mineItem() {
+		// verificar se o item fica inactivo depois de apanhado (durante x tempo)
+		Player p = new Player(true);
+		Skymine s = new Skymine(p.getX(),p.getY(), true);
+
+		assertTrue(s.isActive());
+		s.caught(p, true);
+
+		assertFalse(s.isActive());
+	}
+
+	@Test
+	public void InvulnerabilityItem() {
+		// verificar se o item fica inactivo depois de apanhado (durante x tempo)
+		Player p = new Player(true);
+		Invulnerability i = new Invulnerability();
+
+		assertTrue(i.isActive());
+		i.caught(p, true);
+
+		assertFalse(i.isActive());
+	}
 }
