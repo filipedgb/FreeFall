b924811cda9af01bb3865dbc9fb11b653dfc136a
ei12068 <ei12068@91d1a7ea-e80d-4ec0-ac2e-6a2fed943390>
2014-06-08 15:18:29 +0000

    todos os menus landscape/portrait

--- gen/game/config/R.java
+++ gen/game/config/R.java
@@ -35,8 +35,9 @@ public final class R {
         public static final int malfunc_sprite=0x7f020015;
         public static final int mine=0x7f020016;
         public static final int nuvem=0x7f020017;
-        public static final int satelite=0x7f020018;
-        public static final int slowmo=0x7f020019;
+        public static final int optionslandscape=0x7f020018;
+        public static final int satelite=0x7f020019;
+        public static final int slowmo=0x7f02001a;
     }
     public static final class id {
         public static final int RelativeLayout1=0x7f060024;
